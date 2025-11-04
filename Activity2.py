f0ffeecb83f18fd99d1442b7ab185795a396123f
Satvikai2005 <satvikai2005@gmail.com>
2025-11-03 19:38:47 -0700

    Add files via upload

--- Activity2.py
+++ Activity2.py
@@ -1,6 +1,17 @@
-name = "Skirelex"
-age = 15
+name = "Skrilex"
+a = True
+age = 78
+n1 = 543
 print(name)
+print("Type of name is ", type(name))
+print(a)
+print("Type of a is ", type(a))
 print(age)
-yourname = input("Enter your name:")
-print(f"Hello {yourname}") 
+print("Type of age is ", type(age))
+print(n1)
+print("Type of n1 is ", type(n1))
+age = str(age)
+print(age)
+print("Data Type of age is ", type(age))
+
+
