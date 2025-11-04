688ee631ef2b5b0a1d75731930c3ea4abc6aa210
Satvikai2005 <satvikai2005@gmail.com>
2025-11-03 19:42:54 -0700

    Add files via upload

---
+++ Activity5.py
@@ -0,0 +1,8 @@
+name = "Bob"
+n1 = 55
+n2 = 2.5
+a = False
+print("type of a is ", type(a))
+print("type of n1 is ", type(n1))
+print("type of n2 is ", type(n2))
+print("type of name is ", type(name))
