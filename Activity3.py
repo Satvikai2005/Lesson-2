f4e7dbdadc07f55c1da1e74078008a520f393b92
Satvikai2005 <satvikai2005@gmail.com>
2025-11-03 19:38:59 -0700

    Add files via upload

--- Activity3.py
+++ Activity3.py
@@ -1,3 +1,4 @@
-import keyword
-print("Python keyword's are:")
-print(keyword.kwlist)
+text = input("Enter your name:")
+print("the original text is ", text)
+rev_text = text[::-1]
+print("the reversed text is ", rev_text)
