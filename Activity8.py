80cead00f62fc01b4d302c11fff28f397a4b103a
Satvikai2005 <satvikai2005@gmail.com>
2025-11-10 17:44:24 -0700

    Add files via upload

---
+++ Activity8.py
@@ -0,0 +1,7 @@
+print("Enter marks of any four subjects: ")
+subject1 = int(input("Subject 1: "))
+subject2 = int(input("Subject 2: "))
+subject3 = int(input("Subject 3: "))
+subject4 = int(input("Subject 4: "))
+per = (subject1+subject2+subject3+subject4)/400*100
+print(per)
