98dfb90dcec6406c10dfd7da55e260498d4f6d20
Satvikai2005 <satvikai2005@gmail.com>
2025-11-10 17:22:56 -0700

    Add files via upload

---
+++ Activity6.py
@@ -0,0 +1,7 @@
+building1 = 78
+building2 = 884
+building3 = 665
+building4 = 955
+building5 = 999
+sum = (building1+building2+building3+building4+building5) / 5 
+print("The avrage height of the buildings is ", sum,) 
