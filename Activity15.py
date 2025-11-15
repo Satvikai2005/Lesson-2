06b23c34ac563012662c152af4657ab4969b637b
Satvikai2005 <satvikai2005@gmail.com>
2025-11-14 17:38:35 -0700

    Add files via upload

---
+++ Activity15.py
@@ -0,0 +1,16 @@
+x = 55
+y = 66 
+z = 0 
+if x and y and z:
+    print("One of them is true.")
+else:
+    print("Everything is false.")
+
+x = 105
+y = 0 
+z = 55
+if x or y or z:
+    print("One of them is true.")
+else:
+    print("None of them is true.")
+    
