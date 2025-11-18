592413f85fd4d813737bd2410ef157f19cea073a
Satvikai2005 <satvikai2005@gmail.com>
2025-11-17 17:31:26 -0700

    Add files via upload

--- Activity17.py
+++ Activity17.py
@@ -3,3 +3,9 @@ if (type(x) is int):
     print(f" {x} is an integer.")
 else:
     print(f"{x} is not an integer.")
+    x = 100
+    y = 150
+    if (x is y):
+        print(f"{x} and {y} are similar.")
+    else:
+        print(f"{x} and {y} are not similiar.")
