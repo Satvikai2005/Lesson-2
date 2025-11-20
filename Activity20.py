1b41429b5e772d66f4428a561a878b64fca7b617
Satvikai2005 <satvikai2005@gmail.com>
2025-11-19 17:24:35 -0700

    Add files via upload

---
+++ Activity20.py
@@ -0,0 +1,14 @@
+x = 5
+y = 3
+z = 7
+v = 4
+o = 8
+experession = (x/y)+v*(y+o)-z
+print(experession)
+
+Name = 'Skrillex'
+age = 15
+if Name=='Skrillex'or Name == "JJ" and age<=10:
+    print("Welcome Skrillex.")
+else:
+    print("Sorry, no kids allowed.")
