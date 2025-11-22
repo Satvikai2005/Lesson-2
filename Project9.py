1db402071f5f0d17c93dbd5bc9fed17a0f5a73c9
Satvikai2005 <satvikai2005@gmail.com>
2025-11-21 19:19:09 -0700

    Add files via upload

---
+++ Project9.py
@@ -0,0 +1,12 @@
+age = input("Enter your age here: ")
+if age>10 and age<20:
+    print("You are allowed to Raj's class.")
+    if age>20:
+        print("You are not allowed to Raj's class.") 
+    else:
+        print("You are allowed to Raj's class.")
+else:
+    print("You are not allowed to Raj's class.")
+
+
+ 
