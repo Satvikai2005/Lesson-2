b3d1df97727059d594c691eb52ca78a042edec66
Satvikai2005 <satvikai2005@gmail.com>
2025-12-10 17:26:29 -0700

    Add files via upload

---
+++ Activity31.py
@@ -0,0 +1,8 @@
+word = input("Enter a word here:")
+letter = input("Enter the letter you want to find appeared: ")
+count = 0
+for i in word:
+    count +=1 
+print(f"The letter appeared {count} times.")
+
+
