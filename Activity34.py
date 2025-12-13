2da6836ea60c4d2880ff65f820dfff306136fb07
Satvikai2005 <satvikai2005@gmail.com>
2025-12-12 17:33:09 -0700

    Add files via upload

---
+++ Activity34.py
@@ -0,0 +1,10 @@
+#accepting the number of rows
+x = int(input("Enter the number of rows you want: "))
+"""
+typing the program
+typing the outer loop
+"""
+for i in range(1, x + 1):
+    for j in range(1, i + 1):
+        print("*",end=" ")
+    print()
