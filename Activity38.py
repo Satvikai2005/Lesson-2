cb18daa79c7881aa65d3676edcd6ed34591bcf47
Satvikai2005 <satvikai2005@gmail.com>
2025-12-15 17:43:32 -0700

    Add files via upload

---
+++ Activity38.py
@@ -0,0 +1,33 @@
+import turtle
+turtle.Screen().bgcolor("Blue")
+turtle.Screen().setup(500,500)
+x = turtle.Turtle()
+x.forward(100)
+x.left(120)
+x.forward(100)
+x.left(120)
+x.forward(100)
+x.penup()
+x.right(150)
+x.forward(50)
+x.pendown()
+x.right(90)
+x.forward(100)
+x.right(120)
+x.forward(100)
+x.right(120)
+x.forward(100)
+turtle.done()
+
+
+
+
+
+
+
+
+
+
+
+
+
