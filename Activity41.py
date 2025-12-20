7893e010feaf7849cba35ed4d957584e15a6eb62
Satvikai2005 <satvikai2005@gmail.com>
2025-12-19 17:21:02 -0700

    Add files via upload

---
+++ Activity41.py
@@ -0,0 +1,21 @@
+def weather_conditions():
+    print(f"The weather is {sunny} in summers.")
+    print(f"The weather is {rainy} in rains.")
+    print(f"The weather is {cold} in winters.")
+sunny = "sunny"
+rainy = "rainy"
+cold = "cold"
+weather_conditions()
+
+
+
+
+
+
+
+
+
+
+
+
+
