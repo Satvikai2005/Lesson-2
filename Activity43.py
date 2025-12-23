5807de0ad03e95783253d78213a26a8fad3f9b80
Satvikai2005 <satvikai2005@gmail.com>
2025-12-22 17:19:01 -0700

    Add files via upload

---
+++ Activity43.py
@@ -0,0 +1,17 @@
+def total_calculation(bill_amount,percentage):
+    total_calculation = bill_amount + (bill_amount * (percentage / 100))
+    return total_calculation
+print("The total bill amount is:", total_calculation(150, 10))
+
+
+
+
+
+
+
+
+
+
+
+
+
