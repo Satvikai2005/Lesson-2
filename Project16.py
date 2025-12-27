dfc239c5a0600b6aa73a26551d6d55240b0020ea
Satvikai2005 <satvikai2005@gmail.com>
2025-12-27 16:39:50 -0700

    Add files via upload

--- Project16.py
+++ Project16.py
@@ -1,15 +1,5 @@
-def shutdown():
-    # Prompting user for input
-    user_input = input("Do you want to shut down the system? (Yes/No): ")
-
-    # Checking the conditions
-    if user_input.lower() == "yes":
-        print("Shutting down")
-    elif user_input.lower() == "no":
-        print("Not shutting down")
-    else:
-        print("Sorry")
-
-# Calling the function
-shutdown()
+bill_amount = int(input("Enter the total bill amount:$"))
+bill_amount_gave = int(input("Enter the amount you gave:$"))
+change = bill_amount_gave - bill_amount
+print(f"Your change is ${change}")
 
