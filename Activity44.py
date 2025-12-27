2dca0d657b35b94334c2a73dbf9789f1a479c957
Satvikai2005 <satvikai2005@gmail.com>
2025-12-26 17:18:08 -0700

    Add files via upload

--- Activity44.py
+++ Activity44.py
@@ -1,12 +1,13 @@
-def cube(number):
-    return number * number * number
-def is_divisible_by_3(number):
-    if number % 3 == 0:
-        return f"Cube of {number} is {cube(number)}."
-    else:
-        return f"{number} is not divisible by 3."
-print(is_divisible_by_3(9))
-print(is_divisible_by_3(10))
+Text = input("Enter some text: ")
+A_found = False
+for i in Text:
+    if i == 'A':
+        A_found = True
+        print("A is found.")
+        break
+if not A_found:
+    print("A is not found.")
+    
 
 
 
