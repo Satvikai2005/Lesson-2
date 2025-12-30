225cdf3a038e82f031ba8afaaa891ecad1f4a3b6
Satvikai2005 <satvikai2005@gmail.com>
2025-12-29 17:59:53 -0700

    Add files via upload

--- Project16.py
+++ Project16.py
@@ -1,15 +1,15 @@
 rows = int(input("Enter the number of rows you want: "))
-space = (rows)**2
+space = (rows)*2
 for i in range(1, rows + 1):
     for j in range(1, i + 1):
-        print("*" ,end=" ")
+        print("*" ,end="")
     #print(space,end="")
-    space = space-(rows-1) 
+    space = space-2 
     #print(space,end="")
     for l in range(space+1):
         print(" ", end="") 
     for k in range(1, i + 1):
-        print("*" ,end=" ")
+        print("*" ,end="")
     print() 
     
     
