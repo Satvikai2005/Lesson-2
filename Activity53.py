a95d67cd6699b7eb6aecc13384b688ec61f1d6f8
Satvikai2005 <satvikai2005@gmail.com>
2026-01-05 17:22:13 -0700

    Add files via upload

---
+++ Activity53.py
@@ -0,0 +1,9 @@
+#printing today's date and time
+from datetime import date,time,datetime
+dait = date.today()
+timme = datetime.now()
+print(dait)
+print(timme)
+print(dait.month)
+print(dait.year)
+print(dait.day)
