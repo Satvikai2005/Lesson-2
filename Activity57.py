55385cef5d20247bf496b53a4c1c9423a52fc142
Satvikai2005 <satvikai2005@gmail.com>
2026-01-07 17:54:54 -0700

    Add files via upload

---
+++ Activity57.py
@@ -0,0 +1,11 @@
+melist = [10, 20, 30, 40, 50]
+sum = 0
+for i in range(len(melist)):
+    sum = sum+melist[i]
+print("The sum is:", sum)
+avarage = sum / len(melist)
+print("The average is:", avarage)
+print("The first element is :", melist[0])
+print("The last element is :", melist[-1])
+
+
