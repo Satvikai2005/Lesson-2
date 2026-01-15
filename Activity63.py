74306fbb6b3caed5a35bd791dabc148ac483fbef
Satvikai2005 <satvikai2005@gmail.com>
2026-01-14 18:01:09 -0700

    Add files via upload

---
+++ Activity63.py
@@ -0,0 +1,4 @@
+Country_code = {'United States of America':'+1','United Kingdom':'+44','Japan':'+81','France':'+33','Germany':'+49'}
+print("The country code for United Sates of America is ",Country_code.get('United States of America', 'Country code not found.'))
+print("The country code for Korea is ",Country_code.get('Korea', 'Country code not found.'))
+print("The country code for United Kingdom is ",Country_code.get('United Kingdom', 'Country code not found.'))
