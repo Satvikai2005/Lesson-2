5b532d188568a2d90f262bbcc820984095fd04b0
Satvikai2005 <satvikai2005@gmail.com>
2026-01-19 17:35:51 -0700

    Add files via upload

---
+++ Activity64.py
@@ -0,0 +1,11 @@
+set1 = {1,2,2,3}
+print(set1)
+set2 = {"Hello", 1, 5.00, "Man!", True }
+print(set2)
+set3 = {1,2,2,4,3,2}
+print(set3)
+set4 = set([1,2,3,2])
+print(set4)
+set5 = {0, 1, 3, 4, 5}
+print(set5.pop())
+print(set5)
