bd0d31c361560d51b85e0c914c8abb59ec59a033
Satvikai2005 <satvikai2005@gmail.com>
2026-02-01 16:46:03 -0700

    Printing odd numbers until user's number and printing a list of fruits with capital letters using list comprehension

--- Project25.py
+++ Project25.py
@@ -1,11 +1,12 @@
-# asking the user to enter a number
-number = int(input("Enter a numer here: "))
+#asking user to enter a number
+number = int(input("Enter a number here:"))
 odd = [x for x in range(1, number+1) if x % 2 != 0]
-# printing the list of odd numbers
 print(f"The list of odd numbers until {number} is {odd}")
 
-# making a list of fruits 
+
+#making a list of fruits
 fruits = ["banana", "apple", "mango", "orange"]
-# converting the first letter of the fruits to capital
+#converting the first letter of the fruits capital
 capital_first_letters = [i.capitalize() for i in fruits]
-print(f"Here is the list of fruits with the first letter capital {capital_first_letters}")
+print(f"This is the list of fruits with the first lettr capital:{capital_first_letters}")
+
