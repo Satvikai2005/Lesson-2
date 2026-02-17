e0ef796fad03c1041a5c57eb311c7f9e6be729ae
Satvikai2005 <satvikai2005@gmail.com>
2026-02-16 19:13:53 -0700

    Add files via upload

---
+++ Project27.py
@@ -0,0 +1,23 @@
+class circle:
+  def __init__(self, radius):
+    self.radius = radius
+def area(radius):
+    area = radius*radius*3.14
+    return f"The are of a circle with radius {radius} is {area}."
+def perimeter(radius):
+    perimeter = 2*3.14*radius
+    return f"The perimeter of the circle with radius {radius} is {perimeter}"
+radius = int(input("Enter the radius of the circle here: "))
+operation = input("What operation do you want to do perimeter of area (p/a) of a circle?: ")
+if operation == "perimeter" or operation == "p":
+   print(perimeter(radius))
+elif operation == "area" or operation == "a":
+   print(area(radius))
+else:
+   print("Enter a valid choice next time.")
+
+  
+
+ 
+
+
