bc899928ed8b97b2db283d868853e2a0ee10e503
Satvikai2005 <satvikai2005@gmail.com>
2026-02-19 11:03:48 -0700

    Refactor Circle class and fix method calls

--- Project27.py
+++ Project27.py
@@ -1,25 +1,30 @@
 class Circle:
-  def __init__(self, radius):
-    self.radius = radius
-  def area(self):
-    area = self.radius*self.radius*3.14
-    return f"The are of a circle with radius {radius} is {area}."
-  def perimeter(self):
-    perimeter = 2*3.14*self.radius
-    return f"The perimeter of the circle with radius {radius} is {perimeter}"
-radius = int(input("Enter the radius of the circle here: "))
-o = 
-operation = input("What operation do you want to do perimeter of area (p/a) of a circle?: ")
-if operation == "perimeter" or operation == "p":
-   print(perimeter())
-elif operation == "area" or operation == "a":
-   print(area())
-else:
-   print("Enter a valid choice next time.")
+    def __init__(self, radius):
+        self.radius = radius
+    
+    def area(self):
+        area = self.radius * self.radius * 3.14
+        return f"The area of a circle with radius {self.radius} is {area}."
+    
+    def perimeter(self):
+        perimeter = 2 * 3.14 * self.radius
+        return f"The perimeter of the circle with radius {self.radius} is {perimeter}."
 
+radius = int(input("Enter the radius of the circle here: "))
+o = Circle(radius)
+
+operation = input("What operation do you want to do perimeter or area (p/a) of a circle?: ")
+
+if operation == "perimeter" or operation == "p":
+    print(o.perimeter())
+elif operation == "area" or operation == "a":
+    print(o.area())
+else:
+    print("Enter a valid choice next time.")
   
 
  
+
 
 
 
